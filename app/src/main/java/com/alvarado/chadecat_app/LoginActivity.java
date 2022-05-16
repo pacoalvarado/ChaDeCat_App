ca6150ccbdd598958ae7b5663cc007f214d68bfc
Mooliner <gerymoliner@gmail.com>
2022-05-16 19:16:05 +0200

    Register and Login

--- app/src/main/java/com/alvarado/chadecat_app/LoginActivity.java
+++ app/src/main/java/com/alvarado/chadecat_app/LoginActivity.java
@@ -1,28 +1,57 @@
 package com.alvarado.chadecat_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView email_log, password_log;
+    Button btnLogin;
 
-    Button btLogin;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button info = findViewById(R.id.btlogin);
-        info.setOnClickListener(new View.OnClickListener() {
+        email_log = findViewById(R.id.email_login);
+        password_log = findViewById(R.id.password_login);
+        btnLogin = findViewById(R.id.btnlogin);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), Perfil2Activity.class);
-                startActivityForResult(intent, 0);
+            public void onClick(View view) {
+                String email = email_log.getText().toString().trim();
+                String password = password_log.getText().toString().trim();
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Done Login", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
             }
         });
+
     }
 
 
--- app/src/main/java/com/alvarado/chadecat_app/MainActivity.java
+++ app/src/main/java/com/alvarado/chadecat_app/MainActivity.java
@@ -1,15 +1,49 @@
 package com.alvarado.chadecat_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.alvarado.chadecat_app.databinding.ActivityPerfil2Binding;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityPerfil2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityPerfil2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarPerfil2.toolbar);
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_favorite)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_perfil2);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_perfil2);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
