c51038abde7b94ee45f9510a0bc6277ae5b08264
Mooliner <gerymoliner@gmail.com>
2022-05-16 16:46:26 +0200

    Register and Login

--- app/src/main/java/com/alvarado/chadecat_app/MainActivity.java
+++ app/src/main/java/com/alvarado/chadecat_app/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-        startActivity(intent);
     }
 }
